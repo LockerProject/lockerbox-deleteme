b282607f3d160edb2c6fe94efb5860db359b63b6
Matt Zimmerman <mdz@singly.com>
2011-12-14 15:37:48 -0800

    If pkg-config is not installed, assume the given library isn't either

--- lockerbox.sh
+++ lockerbox.sh
@@ -56,6 +56,11 @@ check_for() {
 
 # check_for_pkg_config name pkg_config_name [minimum_version [optional]]
 check_for_pkg_config() {
+    if ! which pkg-config >/dev/null 2>&1; then
+        echo "pkg-config is not installed: assuming $1 is not present"
+        return 1
+    fi
+
     if ! pkg-config --exists "$2"
     then
         echo "$1 not found!" >&2
